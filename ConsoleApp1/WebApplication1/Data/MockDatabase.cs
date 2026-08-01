175becf01334234790c12ffea0b2832f94cfd0ab
KevinRoque1 <kcndacc@gmail.com>
2026-08-02 01:38:08 +0800

    1st push

---
+++ ConsoleApp1/WebApplication1/Data/MockDatabase.cs
@@ -0,0 +1,29 @@
+﻿using PlaylistApp.Models.Entities;
+
+namespace PlaylistApp.Data
+{
+    public static class MockDatabase
+    {
+        public static List<Playlist> Playlists { get; set; } = new List<Playlist>
+        {
+            new Playlist
+            {
+                Title = "Chill OPM Acoustic Jam",
+                CreatorName = "System",
+                Videos = new List<VideoItem>
+                {
+                    new VideoItem
+                    {
+                        YouTubeUrl = "https://youtu.be/...",
+                        TrackNote = "Classic Eraserheads cover"
+                    },
+                    new VideoItem
+                    {
+                        YouTubeUrl = "https://youtu.be/...",
+                        TrackNote = "Great vocal harmony"
+                    }
+                }
+            }
+        };
+    }
+}
